8e278815a31231189706461f6062c1dd6c26b177
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-03-01 16:45:51 +0100

    fine esercizio  mail

--- mail/js/javascript.js
+++ mail/js/javascript.js
@@ -1,1 +1,20 @@
-alert("lmao")
+//chiedo ad utente mail
+var mailutente = prompt("quale è la tua mail?");
+//le mail che ho nel server
+var listamail = ["shdssba@gmail.com", "bfhdwbfwdb@gmail.com", "fjsfh@gmail.com", "utentemail.gmail.com"];
+var signal;
+//ciclo nell'array
+for (var i = 0; i < listamail.length; i++) {
+  if (listamail[i] == mailutente) {
+    signal = 1;
+    break;
+  } else {
+    signal = 0;
+  }
+}
+//faccio passare
+if ( signal == 1 ) {
+  document.getElementById("responso").innerHTML = "Sei nella Lista Brdi"
+} else {
+  document.getElementById("responso").innerHTML = "Sei un Perdente non puoi entrere qui LMAO"
+}
